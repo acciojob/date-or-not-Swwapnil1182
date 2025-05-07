ba49e5b7f011f19279849fdf9b6d2d460574661f
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-07 20:27:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var isDate = function (input) {
   //   write your code here
+	return typeof(input)
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
+//const input = prompt("Enter Date.");
 alert(isDate(input));
