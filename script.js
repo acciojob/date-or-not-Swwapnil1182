4f6efc821c0df6b098332b1b9191ed914450cd9d
Swwapnil1182 <swapniljadhav1182@gmail.com>
2025-05-07 20:30:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var isDate = function (input) {
   //   write your code here
-	return typeof(input)
+	if(typeof input == date) {
+		return true
+	}
+	return false
 };
 
 // Do not change the code below.
